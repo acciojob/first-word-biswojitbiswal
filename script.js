29cab4ce2e512338d15e067d385db8e62ace817f
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-10-24 11:25:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function firstWord(s) {
   // your code here
+	if(s.length == 0){
+		return '';
+	}
+	s = s.trim();
+	for(let i = 0; i < s.length; i++){
+		if(s.charAt(i) == ' '){
+			return s.slice(0, i);
+		}
+	}
+	return s;
+	
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+const s = prompt("Enter String:");  
 alert(firstWord(s));
